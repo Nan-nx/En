e813232757c6653f88ce19b6be4c53b51df19a73
En <61365467+Nan-nx@users.noreply.github.com>
2022-04-19 11:26:00 +0800

    Add files via upload

---
+++ Script/From Cuttlefish/weixin110.js
@@ -0,0 +1,28 @@
+
+
+var str = ($response.body);
+
+str = str.match(/:&#x2f;&#x2f;(\S*)"}/)[1].replace(/&#x2f;/g, '/').replace(/&amp;/g, '&').split("\"")[0]
+let opener = str.indexOf("m.tb.cn") != -1 ? "taobao://" + str: ($response.body)
+//console.log(str);
+
+const $ = new cmp()
+
+if (str.indexOf("m.tb.cn") != -1) {
+    $.notify(``, "", "🛍️点击打开淘宝", opener)
+} else if (str.indexOf("如需浏览")) {
+    $.notify(``,"", "🔗点击打开链接", "https://"+str)
+}
+
+$done({body: $response.body});
+
+function cmp() {
+    _isQuanX = typeof $task != "undefined"
+    _isLoon = typeof $loon != "undefined"
+    _isSurge = typeof $httpClient != "undefined" && !_isLoon
+    this.notify = (title, subtitle, message, url) => {
+        if (_isLoon) $notification.post(title, subtitle, message, url)
+        if (_isQuanX) $notify(title, subtitle, message, { "open-url": url })
+        if (_isSurge) $notification.post(title, subtitle, message, { url: url })
+    }
+}
